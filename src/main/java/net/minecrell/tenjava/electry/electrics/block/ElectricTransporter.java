bac51bdb8235dedf6514cee7d6087bd899b1b845
Minecrell <dev@minecrell.net>
2014-07-12 16:30:01 +0200

    Oops. Missing abstract for ElectricTransporter!

--- src/main/java/net/minecrell/tenjava/electry/electrics/block/ElectricTransporter.java
+++ src/main/java/net/minecrell/tenjava/electry/electrics/block/ElectricTransporter.java
@@ -1,4 +1,4 @@
 package net.minecrell.tenjava.electry.electrics.block;
 
-public class ElectricTransporter extends ElectricBlock {
+public abstract class ElectricTransporter extends ElectricBlock {
 }
