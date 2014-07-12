f8ee2cad994dce481105ebb82ff9f64bac2bd4b2
Minecrell <dev@minecrell.net>
2014-07-12 11:41:08 +0200

    Add enable / disable message.

--- src/main/java/net/minecrell/tenjava/electry/Electry.java
+++ src/main/java/net/minecrell/tenjava/electry/Electry.java
@@ -5,11 +5,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Electry extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        // For the people complaining about such messages: CraftBukkit does only display that it IS enabling the
+        // plugin and not that the plugin was SUCCESSFULLY enabled. Therefore this message just says that the
+        // configuration and everything was successfully loaded and not only the plugin.
+        this.getLogger().info(this.getDescription().getFullName() + " enabled.");
     }
 
     @Override
     public void onDisable() {
-
+        this.getLogger().info(this.getDescription().getFullName() + " disabled.");
     }
 }
