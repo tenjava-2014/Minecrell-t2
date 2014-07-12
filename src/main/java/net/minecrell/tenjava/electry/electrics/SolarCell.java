23ba4bb2973861adf3f59fa19e7813206264aa1e
Minecrell <dev@minecrell.net>
2014-07-12 13:27:42 +0200

    Add light level to solar cell.

--- src/main/java/net/minecrell/tenjava/electry/electrics/SolarCell.java
+++ src/main/java/net/minecrell/tenjava/electry/electrics/SolarCell.java
@@ -3,5 +3,9 @@ package net.minecrell.tenjava.electry.electrics;
 import net.minecrell.tenjava.electry.electrics.base.ElectricGenerator;
 
 public class SolarCell extends ElectricGenerator {
+    private int lightLevel;
 
+    public int getLightLevel() {
+        return lightLevel;
+    }
 }
